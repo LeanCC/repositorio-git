8fb236e417859768812dc78e0391137705f4cc07
LeanCC <leanc.c@hotmail.com>
2022-11-15 15:45:56 -0300

    Modificaciones
    Se modifica el método de calcular área y se agrega el printeo del objeto.

--- Clases/main.py
+++ Clases/main.py
@@ -8,28 +8,38 @@ PI = math.pi
 if __name__ == "__main__":
 
   class Circulo:
-    def __init__(self,radio,diametro) -> None:
-     self.radio = radio
-     self.diametro = diametro
+    def __init__(self,nombre,radio,diametro) -> None:
+      self.nombre = nombre
+      self.radio = radio
+      self.diametro = diametro
 
     """ Métodos """
     def calc_area(self):
-      return (self.radio**2)*PI
+      try:
+        if self.radio <= 0:
+          raise Exception('ERROR')
+        return (self.radio**2)*PI
+      except Exception:
+        print('ERROR EL VALOR DEL RADIO DEL CIRCULO NO PUEDE SER IGUAL O MENOR A 0')
 
 
     def calc_perimetro(self):
       return self.diametro*PI
 
+    def __str__(self) -> str:
+      return (f'{self.nombre}' + f' es un circulo con un radio de: {self.radio}, y un diametro de: {self.diametro}')
+
 
 """ Pedir el valor del radio y diametro del circulo"""
 
+nombre = input('Ingrese el nombre del circulo: ')
 radio = float(input('Ingrese el valor del radio: '))
 diametro = float(input('ingrese el valor del diametro: '))   
 
 
 """ Instanciar """
 
-c = Circulo(radio,diametro)
+c = Circulo(nombre,radio,diametro)
 
 """ Llamando a los metodos"""
 area = c.calc_area()
@@ -37,3 +47,5 @@ perimetro = c.calc_perimetro()
 
 print(f'El area del circulo es: {area}')
 print(f'El perimetro del circulo es: {perimetro}')
+
+print(c)
