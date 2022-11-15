1b1a388e275ee7efc9c51afe0bd73e949c84a65d
LeanCC <leanc.c@hotmail.com>
2022-11-15 11:24:13 -0300

    Agregamos función
    Se agrega la función para buscar la secuencia horizontal de números consecutivos

--- Matriz/main.py
+++ Matriz/main.py
@@ -36,3 +36,34 @@ for i in result:
 
 print()
 
+
+"""
+Crear función para buscar la secuencia de números de manera horizontal
+
+"""
+
+def consecutive_horizont():
+  if row1[0] == 1 and row1[1] == 2 and row1[2] == 3 and row1[3] == 4:
+    print('Posicion Incial: Fila 1,Elemento 1.\n'
+          'Posicion Final: Fila 1,Elemento 4\n') 
+
+  elif row2[0] == 1 and row2[1] == 2 and row2[2] == 3 and row2[3] == 4:
+    print('Posicion Incial: Fila 2,Elemento 1.\n'
+          'Posicion Final: Fila 2,Elemento 4\n') 
+
+  elif row3[0] == 1 and row3[1] == 2 and row3[2] == 3 and row3[3] == 4:
+    print('Posicion Incial: Fila 3,Elemento 1.\n'
+          'Posicion Final: Fila 3,Elemento 4\n') 
+
+  elif row4[0] == 1 and row4[1] == 2 and row4[2] == 3 and row4[3] == 4:
+    print('Posicion Incial: Fila 4,Elemento 1.\n'
+          'Posicion Final: Fila 4,Elemento 4\n') 
+
+  elif row5[0] == 1 and row5[1] == 2 and row5[2] == 3 and row5[3] == 4:
+    print('Posicion Incial: Fila 5,Elemento 1.\n'
+          'Posicion Final: Fila 5,Elemento 4\n') 
+
+""" Ejecutamos la función"""
+
+consecutive_horizont()
+
